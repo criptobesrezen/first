86fe492f68b5ffa92156c6d16a2c93b2e6ff32e5
Roman Nosov <roman.nosov1991@gmail.com>
2017-10-19 21:43:03 +0400

    'master

--- index.php
+++ index.php
@@ -5,12 +5,28 @@
     <title>Сайт Носова Р.Л. на PHP</title>
 </head>
 <body>
-    <div class="main-nav">
-        <ul class="main-nav__ul">
-            <li><a href="/">Главная</a></li>
+
+<div class="main-nav">
+    <section class="wrapper main-nav__container">
+        <span><p class="logo">CRiPTo BeSReZeN</p></span>
+        <ul class="menu">
+            <li><a class="menu__button" href="/">Главная</a></li>
+            <li><a class="menu__button" href="/">Файлы</a></li>
+            <li><a class="menu__button" href="/">Создание</a></li>
+            <li><a class="menu__button" href="/">Удаление</a></li>
+            <li><a class="menu__button" href="/">Редактирование</a></li>
         </ul>
+    </section>
+</div>
+<section class="wrapper">
+    <div class="content">
+        <h1 class="content__header">Добро пожаловать на мой сайт</h1>
     </div>
+</section>
+
+
 
 <link rel="stylesheet" href="style.css">
+<link href="src/css/poiret-one-font.css" rel="stylesheet">
 </body>
 </html>
